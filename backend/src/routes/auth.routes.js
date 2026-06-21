eaff8217fb0bd6da8e8300489eb3c194d82332d0
Abhishek <abhi.l127375@gmail.com>
2026-06-22 00:00:42 +0530

    Added some changes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,6 @@ const User = require('../models/User.model');
 const { login, changePassword } = require('../controllers/auth.controller');
 const validate = require('../middleware/validate');
 const { loginRules } = require('../middleware/leadValidators');
-const { loginLimiter } = require('../middleware/rateLimiters');
 
 router.post('/login', loginLimiter, loginRules, validate, login);
 const auth = require('../middleware/auth');
